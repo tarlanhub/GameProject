6cd4d079c3d5567d3ceff83d6d14302d8abe1b93
pdesgn <pdesgn@DESKTOP-LC4MO2N>
2021-03-05 02:43:29 +0300

    Add project files.

---
+++ GameProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 1998,
+                FirstName = "Tarlan",
+                LastName = "Abdullayev",
+                IdentityNumber = 12345
+            });
+           
+                
+                Console.WriteLine("Teşekkürler");
+        }
+    }
+}
